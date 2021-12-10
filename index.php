98e4afebdd17eeb8c2f2d65ce50aadc44ebfba8e
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2021-12-09 22:21:40 -0700

    Nav bar trial

--- index.php
+++ index.php
@@ -7,14 +7,26 @@
   <link rel = "icon" href =
   "WIN_20210622_19_02_08_Pro (2).jpg"
           type = "image/x-icon">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 
 
-<body>  
-  <?php echo "<h1>Hello World! (CSS has changed this text to blue!)</h1>" ?>  
-  <img src="img/thedude.jpg" alt="The dude">
-  <?php include("example.php"); ?>
-  <button type="button" onclick="helloJS()">Click Me!</button>
+<body>
+  <div class="topnav" id="myTopnav">
+    <a href="#home" class="active">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
+
+  <div style="padding-left:16px">
+    <h2>Responsive Topnav Example</h2>
+    <p>Resize the browser window to see how it works.</p>
+  </div>
   
 </body>
   <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 function helloJS()
 {
     alert("The Dude Abides FOR REAL! (this is from the file js/main.js)");
+
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
