bdd14b31b5565c55f821b3b44ea875f1028fafea
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2021-12-09 23:14:23 -0700

    Style wipe

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
   <link rel = "icon" href =
   "WIN_20210622_19_02_08_Pro (2).jpg"
           type = "image/x-icon">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
@@ -15,7 +14,6 @@
 <body>
   <div class="topnav" id="myTopnav">
     <a href="#home" class="active">Home</a>
-    <a href="#news">News</a>
     <a href="#contact">Contact</a>
     <a href="#about">About</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
