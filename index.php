6984a78afc95aa48111380b4ad8f8725c0f3a2c6
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2021-12-11 14:03:24 -0700

    update logo

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 
 <body>
   <div class="navbar" id="navId">
+    <img src="Blogapalooza-logo.jpeg" alt="logo" />
     <a href="#home" class="active">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
