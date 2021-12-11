3a1f6bc54e3835d60ded89b8af5f9d89000b7ce6
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2021-12-10 17:28:08 -0700

    blog template2

--- index.php
+++ index.php
@@ -25,15 +25,11 @@
   </div>
 
   <div class="row">
-    <div class="leftcolumn">
-      <div class="card">
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Dec 7, 2017</h5>
-        <div class="fakeimg" style="height:200px;">Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-      </div>
-    </div>
+    <form action="/action_page.php">
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname"><br><br>
+      <input type="submit" value="Submit">
+    </form>
   </div>
 
   <div class="footer">
