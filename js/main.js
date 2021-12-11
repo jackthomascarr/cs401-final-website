02f087355fae01fb77608e022b22dbc278b9e3b2
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2021-12-11 12:47:17 -0700

    hot fix

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function createNewBlog() {
 
 function getInputValue(){
     // Selecting the input element and get its value
-    var inputVal = document.getElementById("myInput").value;
+    var inputVal = document.getElementById("postButton").value;
 
     // Displaying the value
     alert(inputVal);
