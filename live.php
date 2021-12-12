a76ae80e14206faa62bc3f1d70a7439a1c5dcfe8
Jack Carr <Jack.Carr@mduresources.com>
2021-12-11 19:14:35 -0700

    nav bar

--- live.php
+++ live.php
@@ -15,8 +15,8 @@
 <div class="navbar" id="navId">
     <img src="Blogapalooza (2).jpeg" alt="logo" width="45%" height="45%" style="float:left"/>
     <a href="contact.php">About this site</a>
-    <a href="#live_posts">Live Posts</a>
-    <a href="#create_section" class="active">Home</a>
+    <a href="live.php">Live Posts</a>
+    <a href="index.php" class="active">Home</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     </a>
 </div>
