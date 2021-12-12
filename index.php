026488bb690a0e4d2fcaa2e7d8846c75f458ef27
Jack Carr <Jack.Carr@mduresources.com>
2021-12-11 21:11:36 -0700

    center

--- index.php
+++ index.php
@@ -21,21 +21,18 @@
      </a>
   </div>
 
-  <div class = "center">
   <div class="center">
       <input type="button" name="create" id="createButton" class="createButton" value="Create New Post" onclick="createNewBlog()" />
   </div>
   <br>
   <div id="createBlog"  style="display:none; width:800px; margin:0 auto;" class="center" >
-      <h3 style="text-align:center">Tell us about the time you did that thing!</h3>
+      <h3 style="text-align:center; font-family: Arial, Helvetica, sans-serif;">Tell us about the time you did that thing!</h3>
 
         <input type="text" id="title" name="title" placeholder="Enter a Funky Title Here!"><br>
         <textarea name="textbox" id="textbox" cols="40" rows="5"></textarea>
         <br><br>
         <button id="postButton" class="postButton" onclick="getInputValue();">Post to Blogapalooza!</button>
 
-
-  </div>
   </div>
 
 
